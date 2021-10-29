51ce8461429e68aacb2459664d0886310b7f6a9e
ilya <volchenko.ilya7@gmail.com>
2021-10-29 19:11:51 +0800

    portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  
+  
+$('.skills__slider-inner').slick({
+  infinite: true,
+  // slidesToShow: 4,
+  // slidesToScroll: 4,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true
+});
+
+});
